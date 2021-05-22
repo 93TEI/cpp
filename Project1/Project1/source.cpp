1d07e98769bff86e1be8246fc1750da34755627a
93TEI <93louis@gmail.com>
2021-05-22 23:21:20 +0900

    no message

--- Project1/Project1/source.cpp
+++ Project1/Project1/source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "header.h"
 
 #define TEI
@@ -37,7 +38,7 @@ int main()
 	const unsigned char isDead = 1 << 6; // 0100 0000 
 	const unsigned char isCrying = 1 << 7; // 1000 0000 
 	// 하지만 std::bitset을 이용할것. 오류를 막기위해
-	*/
+	
 	//RGB 표현
 	const unsigned int redBits = 0xFF000000;
 	const unsigned int greenBits = 0x00FF0000;
@@ -57,6 +58,35 @@ int main()
 	std::cout << static_cast<int>(green) << "of 255 red\n";
 	std::cout << static_cast<int>(blue) << "of 255 red\n";
 	std::cout << static_cast<int>(alpha) << "of 255 red\n";
+	*/
+
+	int i = 100;
+	i = i / 2.3;
+	std::cout << i << endl;
+	i = 100;
+	i = static_cast<int>(i / 2.3);
+	std::cout << i << endl;
+
+	//string
+	std::string myName("person1");
+	std::string yourName;
+	std::cin >> yourName;
+	std::cin.ignore(32767, '\n'); // 버퍼에 남은 \n 제거되는 코드, 32767은 무시할 문자 수.
+	std::cout << yourName << endl;
+	std::getline(std::cin, yourName); // 한줄 전체 받기
+	std::cout << yourName << endl;
+	std::cout << (myName.length() > yourName.length() ? myName : yourName) << endl;
+
+	//열거형
+	enum PERSON
+	{
+		kim, //자동으로 0 할당 / 물론 다른 값으로 값을 줄 수 있다. 그 뒤는 1씩 증가된 값을 같게 됨.
+		lee, // 1할당
+		park // 2할당
+	};
+
+	PERSON new_person(kim);
+	std::cout << new_person << endl; // 0이 출련된다
 
 	return 0;
 }
